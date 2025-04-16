64af26b26fdf65fc3083ecfcd2e469205480a3a1
WhyK <windchimeyk@gmail.com>
2025-04-17 01:27:18 +0900

    feat: count stop

--- mod.tsx
+++ mod.tsx
@@ -1,46 +1,26 @@
 import { Hono } from "@hono/hono";
 import { Style } from "@hono/hono/css";
-import {
-  convertElapsedDateTime,
-  getNowEpisodeText,
-  incrementEpisodeNum,
-  LAST_EPISODE_DATE,
-  LAST_EPISODE_NUM,
-} from "~/core.ts";
-import {
-  buttonStyle,
-  buttonWrapperStyle,
-  headerStyle,
-  mainStyle,
-  nowEpisodeTextStyle,
-  titleStyle,
-} from "~/mod.css.ts";
+import { serveStatic } from "@hono/hono/deno";
+import { headerStyle, mainStyle, titleStyle } from "~/mod.css.ts";
 
 const SITE_NAME = "今、リコリコって何話だっけ？";
 const SITE_DESCRIPTION = "リコリコの最新話(？)がわかるだけのサイトです";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  const now = new Date().toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
-  const nowEpisodeNum = incrementEpisodeNum(
-    LAST_EPISODE_NUM,
-    convertElapsedDateTime(now, LAST_EPISODE_DATE, "date"),
-  );
-  const nowEpisodeText = getNowEpisodeText(
-    now,
-    nowEpisodeNum,
-  );
+app.use("/static/*", serveStatic({ root: "./" }));
 
+app.get("/", (c) => {
   return c.html(
     <html>
       <head>
         <title>{SITE_NAME}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="descrition" content={SITE_DESCRIPTION} />
+        <link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css" />
         <link
-          href="https://cdn.jsdelivr.net/npm/modern-normalize@2.0.0/modern-normalize.min.css"
           rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
         />
         <Style />
       </head>
@@ -49,17 +29,19 @@ app.get("/", (c) => {
           <h1 class={titleStyle}>{SITE_NAME}</h1>
         </header>
         <main class={mainStyle}>
-          <p class={nowEpisodeTextStyle}>{nowEpisodeText}</p>
-          <div class={buttonWrapperStyle}>
-            <a
-              class={buttonStyle}
-              href={`https://twitter.com/intent/tweet?text=${nowEpisodeText}&url=%0a%0ahttps://lycoreco-now.deno.dev%0a&hashtags=今リコリコって何話だっけ,リコリコ${nowEpisodeNum}話`}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              ツイートする
-            </a>
-          </div>
+          <img src="/static/image.png" alt="" />
+          <p>
+            リコリコ第1期146話目(？)にして、新作アニメーションが放送されました。<br />それに伴って、カウントを停止しました。
+          </p>
+          <p>放送おめでとうございます🎉🎉🎉🎉</p>
+          <section>
+            <h2>各話リンク</h2>
+            <ul>
+              <li>
+                <a href="https://youtu.be/z4lNa68phLs">#1</a>
+              </li>
+            </ul>
+          </section>
         </main>
       </body>
     </html>,
